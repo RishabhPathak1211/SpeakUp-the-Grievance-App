0f7d0ce16dfc3e7e91a20f8c293718b4992b5635
RishabhPathak1211 <rishabh2000.pathak@gmail.com>
2020-10-01 09:01:48 +0530

    moved code

---
+++ Web App/flaskprac.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('D:\Third semester project\home.html')
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}"
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('home'))
+
+if  __name__ == "__main__":
+    app.run()

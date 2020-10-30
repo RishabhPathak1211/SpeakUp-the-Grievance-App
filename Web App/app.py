a75b426382b0b9817d31832c82df73f6b1b8b090
RishabhPathak1211 <rishabh2000.pathak@gmail.com>
2020-10-30 08:55:51 +0530

    Added category confirmation functions

--- Web App/app.py
+++ Web App/app.py
@@ -10,19 +10,24 @@ from text_classification import classify
 client = pymongo.MongoClient("mongodb+srv://Rishabh_Pathak_12:speakup@cluster0.agd8b.mongodb.net/ThirdSemProj?retryWrites=true&w=majority")
 db = client['ThirdSemProj']
 comps = db['Complaints']
+dataset = db['Dataset']
 
-for x in comps.find({'Category':''}):
+for x in comps.find({'category':''}):
     id_x = x['_id']
-    body = x['Body']
-    comps.update_one({'_id':ObjectId(id_x)}, {'$set': {'Category':classify(body)}})
+    body = x['body']
+    comps.update_one({'_id':ObjectId(id_x)}, {'$set': {'category':classify(body)}})
 
 def counts(col, user):
     count = {'hostelcount': 0, 'academicscount': 0, 'financecount': 0, 'messcount': 0}
-    count['hostelcount'] = col.find({'Category':'Hostel', 'Institution':user}).count()
-    count['academicscount'] = col.find({'Category':'Academics', 'Institution':user}).count()
-    count['financecount'] = col.find({'Category':'Finance', 'Institution':user}).count()
-    count['messcount'] = col.find({'Category':'Mess', 'Institution':user}).count()
+    count['hostelcount'] = col.find({'category':'Hostel', 'institution':user}).count()
+    count['academicscount'] = col.find({'category':'Academics', 'institution':user}).count()
+    count['financecount'] = col.find({'category':'Finance', 'institution':user}).count()
+    count['messcount'] = col.find({'category':'Mess', 'institution':user}).count()
     return count
+
+def initials(name):
+    parts = name.split()
+    return str(parts[0][0]) + str(parts[-1][0])
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -98,9 +103,9 @@ def complaintlist(user, category):
     if user not in session.values():
         return redirect(url_for('login'))
     comp_lst = []
-    for x in comps.find({'Institution':user, 'Category':category}):
+    for x in comps.find({'institution':user, 'category':category}):
         comp_lst.append(x)
-    return render_template('complaint.html', category=category, comp_lst=comp_lst, count=counts(comps, user), user=user)
+    return render_template('complaint.html', category=category, comp_lst=comp_lst, count=counts(comps, user), user=user, initials=initials)
 
 @app.route('/complaint/<user>/<category>/<complaint_id>')
 def complaint(user, category, complaint_id):
@@ -110,11 +115,27 @@ def complaint(user, category, complaint_id):
     rem_categories.remove(category)
     comp_lst = []
     mycomp = []
-    for x in comps.find({'Institution':user, 'Category':category}):
+    for x in comps.find({'institution':user, 'category':category}):
         comp_lst.append(x)
     for x in comps.find({'_id':ObjectId(complaint_id)}):
         mycomp.append(x)
-    return render_template('complaint.html', category=category, comp_lst=comp_lst, count=counts(comps, user), mycomp=mycomp, user=user, temp=0)
+    return render_template('complaint.html', category=category, comp_lst=comp_lst, count=counts(comps, user), mycomp=mycomp, user=user, rem_cats=rem_categories, initials=initials)
+
+@app.route('/confirmed/<complaint_id>')
+def confirmed(complaint_id):
+    x = comps.find_one({'_id':ObjectId(complaint_id)})
+    found = dataset.find_one({'comp':x['body']})
+    if not found:
+        dataset.insert_one({'dept':x['category'], 'comp':x['body']})
+        comps.update_one({'_id':ObjectId(complaint_id)}, {'$set': {'status':True}})
+    return redirect(url_for('complaint', user=session['username'], category=x['category'], complaint_id=complaint_id))
+
+@app.route('/changed/<complaint_id>/<new_category>')
+def changed(complaint_id, new_category):
+    x = comps.find_one({'_id':ObjectId(complaint_id)})
+    if x['category'] != new_category:
+        comps.update_one({'_id':ObjectId(complaint_id)}, {'$set': {'category':new_category}})
+    return redirect(url_for('complaintlist', user=session['username'], category=x['category']))
 
 if  __name__ == "__main__":
     app.run()
